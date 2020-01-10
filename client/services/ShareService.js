44a9af18ee376bca979b0f3507d6d7e913e5c808
Murray Stewart <murray.stewart789@gmail.com>
2020-01-10 14:44:26 +0000

    adding get to SharesService

--- client/services/ShareService.js
+++ client/services/ShareService.js
@@ -1,4 +1,19 @@
-const baseURL = "https://api.worldtradingdata.com/api/v1/stock";
-const key = "QTA1FETX7I0B34WC";
+const baseURL = "https://www.alphavantage.co/query?function=";
+const intraDayQuery = "function=TIME_SERIES_INTRADAY&symbol=";
+const intraDayParams = "&interval=1min&outputsize=compact"
 
-const symbols= ["AAPL","GOOGL","NVDA","AMZN","ATVI","FB","NFLX","SBUX","EXPE","CMCSA"];
+const key = "&api_key=QTA1FETX7I0B34WC";
+
+const tickers= ["AAPL","GOOGL","NVDA","AMZN","ATVI","FB","NFLX","SBUX","EXPE","CMCSA"];
+
+
+export default {
+  getShares(){
+    let promises = [];
+    for(ticker in tickers){
+      promise.push(fetch(baseURL + intraDayQuery + ticker + intraDayParams))
+    }
+
+    Promise.all(promises);
+  }
+}
