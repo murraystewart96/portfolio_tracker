cb4313a641e51182ea62ce4dc2c38be97f9bc173
Murray Stewart <murray.stewart789@gmail.com>
2020-01-10 14:00:22 +0000

    adding seeds

--- client/services/ShareService.js
+++ client/services/ShareService.js
@@ -1,2 +1,4 @@
 const baseURL = "https://api.worldtradingdata.com/api/v1/stock";
-const key = "BoY5TDSlSKMUIzlPbKbvZb6dr7odrltB61YRE0RAwXl3o5HYzrkjrm7TkkYT";
+const key = "QTA1FETX7I0B34WC";
+
+const symbols= ["AAPL","GOOGL","NVDA","AMZN","ATVI","FB","NFLX","SBUX","EXPE","CMCSA"];
---
+++ server/db/seeds.js
@@ -0,0 +1,34 @@
+
+
+const seedData = [
+  {
+    ticker: "AAPL",
+    exchange: "NASDAQ",
+    price: 309.7100,
+    quantity: 30
+  },
+  {
+    ticker: "GOOGL",
+    exchange: "NASDAQ",
+    price: 1420.1500,
+    quantity: 20
+  },
+  {
+    ticker: "NVDIA",
+    exchange: "NASDAQ",
+    price: 243.0000,
+    quantity: 40
+  },
+  {
+    ticker: "AMZN",
+    exchange: "NASDAQ",
+    price: 1900.1400,
+    quantity: 10
+  },
+  {
+    ticker: "NFLX",
+    exchange: "NASDAQ",
+    price: 335.6600,
+    quantity: 15
+  }
+]
