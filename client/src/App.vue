787b99ce1f52febb2fbadc3fc4da13132d623fec
Andrew Binnie <binnieandrew@gmail.com>
2020-01-12 14:26:14 +0000

    adding the totals

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <h1>Portfolio Tracker</h1>
-    <!-- <portfolio-total :shares="shares"/>
-    <share-list :shares="shares"/> -->
+    <portfolio-total :shares="shares"/>
+    <share-list :shares="shares"/>
   </div>
 </template>
 
@@ -17,7 +17,22 @@ export default {
   name: 'app',
   data(){
     return {
-      shares: [],
+      shares: [  {
+        _id: "5e199937dc3127e9ea7607ae",
+        ticker: "AAPL",
+        name: "Apple Inc.",
+        exchange: "NASDAQ",
+        price: 10,
+        quantity: 30
+      },
+      {
+        _id: "5e199985dc3127e9ea7607af",
+        ticker: "GOOGL",
+        name: "Alphabet Inc.",
+        exchange: "NASDAQ",
+        price: 20,
+        quantity: 20
+      }],
       temp: null
 
     }
@@ -30,29 +45,31 @@ export default {
   mounted(){
 
 
-    // SharesService.getShares()
-    // .then(data => {
-    //   this.shares = data;
-    //   SharesService.updateSharePrices(this.shares);
-    // })
-
-    SharesService.getPricesDaily("AAPL")
-    .then(prices => console.log(, prices));
-
-    SharesService.getPricesIntraday("AAPL")
-    .then(prices => console.log("Intradaily Prices", prices));
-
-  },
-
-  methods: {
+    SharesService.getShares()
+    .then(data => {
+      this.shares = data;
+      console.log(data);
+      SharesService.updateSharePrices(this.shares);
+    })
 
 
-
-  },
-
-  components: {
+    // SharesService.getPricesDaily("AAPL")
+    // .then(prices => console.log("Daily Prices", prices));
+    //
+    // SharesService.getPricesIntraday("AAPL")
+    // .then(prices => console.log("Intradaily Prices", prices));
 
   }
+  //
+  // methods: {
+  //
+  //
+  //
+  // },
+  //
+  // components: {
+  //
+  // }
 }
 </script>
 
