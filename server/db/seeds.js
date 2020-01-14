635b5d29b291b9e91cddb567bbfceec0ac6353d1
Andrew Binnie <binnieandrew@gmail.com>
2020-01-14 09:47:07 +0000

    updating the seeds file to speak to database

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,39 +1,36 @@
+use shares_portfolio;
 
+db.dropDatabase()
 
-const seedData = [
+db.shares.insertMany([
   {
-  _id: "5e199937dc3127e9ea7607ae",
-  ticker: "AAPL",
-  name: "Apple Inc.",
-  exchange: "NASDAQ",
-  quantity: 30
+  "ticker": "AAPL",
+  "name": "Apple Inc.",
+  "exchange": "NASDAQ",
+  "quantity": 30
 },
 {
-  _id: "5e199985dc3127e9ea7607af",
-  ticker: "GOOGL",
-  name: "Alphabet Inc.",
-  exchange: "NASDAQ",
-  quantity: 20
+  "ticker": "GOOGL",
+  "name": "Alphabet Inc.",
+  "exchange": "NASDAQ",
+  "quantity": 20
 },
 {
-  _id: "5e1999bbdc3127e9ea7607b0",
-  ticker: "ATVI",
-  name: "Activision Blizzard Inc.",
-  exchange: "NASDAQ",
-  quantity: 50
+  "ticker": "ATVI",
+  "name": "Activision Blizzard Inc.",
+  "exchange": "NASDAQ",
+  "quantity": 50
 },
 {
-  _id: "5e1999eadc3127e9ea7607b1",
-  ticker: "AMZN",
-  name: "Amazon.com Inc.",
-  exchange: "NASDAQ",
-  quantity: 40
+  "ticker": "AMZN",
+  "name": "Amazon.com Inc.",
+  "exchange": "NASDAQ",
+  "quantity": 40
 },
 {
-  _id: "5e199a15dc3127e9ea7607b2",
-  ticker: "NVDA",
-  name: "NVIDIA Corporation",
-  exchange: "NASDAQ",
-  quantity: 40
+  "ticker": "NVDA",
+  "name": "NVIDIA Corporation",
+  "exchange": "NASDAQ",
+  "quantity": 40
 }
-]
+])
